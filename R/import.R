962bc13ef5c2f154b6784fbc0035976ee239f9e1
Bastiaan Quast <bquast@gmail.com>
2016-03-17 11:56:21 +0100

    add import of regional growth stats?

--- R/import.R
+++ R/import.R
@@ -9,6 +9,9 @@ library(dplyr)
 
 
 # download data
+gsloc <- gs_url(x = 'https://docs.google.com/spreadsheets/d/1DWUP8PGtayRN5FwtVPlVa0HSsaMIx_3ETHXGObcB8qo/edit?usp=sharing')
+reggrow <- gs_read(ss = gsloc, range = 'A1:J9')
+
 gsloc <- gs_url(x = 'https://docs.google.com/spreadsheets/d/1e1hy_-3Ndvz-AK69DvroTJgnGQnFEY34IYs_QOO2DiI/edit?usp=sharing')
 uselang <- gs_read(ss = gsloc, range = 'A1:AF215')
 
@@ -17,4 +20,5 @@ uselang <- gs_read(ss = gsloc, range = 'A1:AF215')
 uselang <- uselang[, c(1, 26:32)]
 
 # save data
-save(uselang, file = 'data/uselang')
+save(reggrow, file = 'data/reggrow.RData')
+save(uselang, file = 'data/uselang.RData')
